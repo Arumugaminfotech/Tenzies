84adebd8ac46447cd9c4193b7489cb87946c48e3
Arumugam <arumugam.s@tringapps.com>
2023-11-28 19:22:11 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ export default function App() {
     
     return (
         <main>
-            <h1 className="title">Tenzies</h1>
+            <h1 className="title">Tenziess</h1>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
